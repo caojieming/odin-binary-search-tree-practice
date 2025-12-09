f576ec28b3a28d70070f79a01f13f5afe748856b
James Cao <jcao5360@gmail.com>
2025-12-09 17:13:04 -0500

    up to idBalanced, debugging not every node being traversed

--- index.js
+++ index.js
@@ -109,42 +109,208 @@ class Tree {
 
 
     find(value) {
-
+        return this.findNode(this.root, value);
+    }
+    findNode(currNode, value) {
+        // base case, reached a null
+        if(currNode === null) {
+            return null;
+        }
+        else {
+            // value equals currNode, found node
+            if(value === currNode.value) {
+                return currNode;
+            }
+            // value is less than currNode, go left
+            else if(value < currNode.value) {
+                return this.findNode(currNode.left, value);
+            }
+            // value is greater than currNode, go right
+            else {
+                return this.findNode(currNode.right, value);
+            }
+        }
     }
 
 
     levelOrderForEach(callback) {
+        // 2d array, first index is the layer/level of node in the BST (starting at 0 for the root), second index is from left to right order within the level
+        const levelList = [];
 
+        // directly generates bfs list in levelList
+        this.bfs(this.root, 0, levelList);
+
+        // flatten levelList into 1d array, then apply callback function to all nodes
+        const levelOrder = levelList.flat();
+        levelOrder.forEach((e) => callback(e));
+    }
+    bfs(currNode, currLevel, levelList) {
+        // base case, stop/do nothing
+        if(currNode === null) {
+            return;
+        }
+        else {
+            // starting a new level, so create a new list
+            if(currLevel >= levelList.length) {
+                levelList.push([]);
+            }
+            // add currNode to levelList
+            levelList[currLevel].push(currNode);
+            // traverse the next level
+            this.bfs(currNode.left, currLevel + 1, levelList);
+            this.bfs(currNode.right, currLevel + 1, levelList);
+        }
     }
 
 
     inOrderForEach(callback) {
-
+        this.dfsInOrder(this.root, callback);
+    }
+    dfsInOrder(currNode, callback) {
+        // base case, stop/do nothing
+        if(currNode === null) {
+            return;
+        }
+        else {
+            this.dfsInOrder(currNode.left, callback);
+            callback(currNode);
+            this.dfsInOrder(currNode.right, callback);
+        }
     }
 
 
     preOrderForEach(callback) {
-
+        this.dfsPreOrder(this.root, callback);
+    }
+    dfsPreOrder(currNode, callback) {
+        // base case, stop/do nothing
+        if(currNode === null) {
+            return;
+        }
+        else {
+            callback(currNode);
+            this.dfsPreOrder(currNode.left, callback);
+            this.dfsPreOrder(currNode.right, callback);
+        }
     }
 
 
     postOrderForEach(callback) {
-
+        this.dfsPostOrder(this.root, callback);
+    }
+    dfsPostOrder(currNode, callback) {
+        // base case, stop/do nothing
+        if(currNode === null) {
+            return;
+        }
+        else {
+            this.dfsPostOrder(currNode.left, callback);
+            this.dfsPostOrder(currNode.right, callback);
+            callback(currNode);
+        }
     }
 
 
     height(value) {
-
+        // height is initially -inf so that incrementing it does nothing, and will stay doing nothing until the value has been found, at which point height is set to 0 and increments normally
+        return this.findHeight(this.root, value, Number.NEGATIVE_INFINITY);
+    }
+    findHeight(currNode, value, height) {
+        // base case, reached a null/leaf
+        if(currNode === null) {
+            // if height is negative (infinity), that means the value was never found, so return a null
+            if(height < 0) {
+                return null;
+            }
+            return height;
+        }
+        else {
+            // value equals currNode, found node, set height to 0 so it can start incrementing normally
+            if(value === currNode.value) {
+                return Math.max(this.findHeight(currNode.left, value, 0), this.findHeight(currNode.right, value, 0));
+            }
+            // value is less than currNode, go left
+            else if(value < currNode.value) {
+                return this.findHeight(currNode.left, value, height + 1);
+            }
+            // value is greater than currNode, go right
+            else {
+                return this.findHeight(currNode.right, value, height + 1);
+            }
+        }
     }
 
 
     depth(value) {
-
+        return this.findDepth(this.root, value, 0);
+    }
+    findDepth(currNode, value, depth) {
+        // base case, reached a null/leaf without finding value
+        if(currNode === null) {
+            return null;
+        }
+        else {
+            // value equals currNode, found node
+            if(value === currNode.value) {
+                return depth;
+            }
+            // value is less than currNode, go left
+            else if(value < currNode.value) {
+                return this.findDepth(currNode.left, value, depth + 1);
+            }
+            // value is greater than currNode, go right
+            else {
+                return this.findDepth(currNode.right, value, depth + 1);
+            }
+        }
     }
 
 
     isBalanced() {
+        return this.checkBalance(this.root);
+    }
+    checkBalance(currNode) {
+        // base case, null is technically balanced
+        if(currNode === null) {
+            return true;
+        }
+        else {
+            // leaf, balanced by default
+            if(currNode.left === null && currNode.right === null) {
+                return true;
+            }
+            // left branch only, true if its height <= 1
+            else if(currNode.right === null) {
+                const leftNode = currNode.left;
+                return this.findHeight(leftNode, leftNode.value, 0) <= 1;
+            }
+            // right branch only, true if its height <= 1
+            else if(currNode.left === null) {
+                const rightNode = currNode.right;
+                return this.findHeight(rightNode, rightNode.value, 0) <= 1;
+            }
+            // 2 branches, true if the 2 branch heights have a difference <= 1
+            else {
+                const leftNode = currNode.left;
+                const rightNode = currNode.right;
 
+                // check current node's branches are balanced
+                const leftHeight = this.findHeight(leftNode, leftNode.value, 0);
+                const rightHeight = this.findHeight(rightNode, rightNode.value, 0);
+                const balanced = Math.abs(leftHeight - rightHeight) <= 1;
+
+                console.log(currNode.value);
+                console.log(leftHeight);
+                console.log(rightHeight);
+                console.log();
+
+                // recurse through rest of tree to check if rest of nodes are balanced
+                const leftCheckBal = this.checkBalance(currNode.left);
+                const rightCheckBal = this.checkBalance(currNode.right);
+
+                return balanced && leftCheckBal && rightCheckBal;
+            }
+        }
     }
 
 
--- test.js
+++ test.js
@@ -23,3 +23,57 @@ prettyPrint(tree.root);
 tree.deleteItem(8);
 prettyPrint(tree.root);
 console.log();
+
+
+console.log("finding node with value 324");
+console.log(tree.find(324));
+console.log();
+
+
+console.log("levelOrderForEach((e) => console.log(e.value)), order should be: 9 4 324 1 5 23 6345 3 7");
+tree.levelOrderForEach((e) => console.log(e.value));
+console.log();
+
+
+console.log("inOrderForEach((e) => console.log(e.value)), order should be ascending numerical order");
+tree.inOrderForEach((e) => console.log(e.value));
+console.log();
+
+
+console.log("preOrderForEach((e) => console.log(e.value)), order should be: 9 4 1 3 5 7 324 23 6345");
+tree.preOrderForEach((e) => console.log(e.value));
+console.log();
+
+
+console.log("postOrderForEach((e) => console.log(e.value)), order should be: 3 1 7 5 4 23 6345 324 9");
+tree.postOrderForEach((e) => console.log(e.value));
+console.log();
+
+
+console.log("height() for 9, 4, 5, 7, 11: should return 3, 2, 1, 0, null");
+console.log(tree.height(9));
+console.log(tree.height(4));
+console.log(tree.height(5));
+console.log(tree.height(7));
+console.log(tree.height(11));
+console.log();
+
+
+console.log("depth() for 9, 4, 5, 7, 11: should return 0, 1, 2, 3, null");
+console.log(tree.depth(9));
+console.log(tree.depth(4));
+console.log(tree.depth(5));
+console.log(tree.depth(7));
+console.log(tree.depth(11));
+console.log();
+
+
+console.log("isBalanced(): true");
+console.log(tree.isBalanced());
+console.log("add a 9999 and 99999 to unbalance tree");
+tree.insert(9999);
+tree.insert(99999);
+prettyPrint(tree.root);
+console.log("isBalanced(): false");
+console.log(tree.isBalanced());
+console.log();
